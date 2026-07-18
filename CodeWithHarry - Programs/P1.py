75606baf5aff2184f579ffc400eb316bad310ece
visudevin <visharise@gmail.com>
2026-07-19 01:29:33 +0530

    feat: Program 1 From CodewithHarry Yt Lec

---
+++ CodeWithHarry - Programs/P1.py
@@ -0,0 +1,11 @@
+#please dont remove this line
+"""hello  dont
+hello """
+print("Print with visu", "dont be serious", end=" ")
+print("Designer")
+print("C:\"visu")
+print("C:\\nvisu")
+print("C:\\visu")
+print("visu is a \ngood boy \t1") #comment star here from new line
+
+#learning escape sequence and comment
